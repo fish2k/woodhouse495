51f08a2a05269971e0a6f1547bf743d6e28646c3
Burhanov Ildar <burhanoff1992@gmail.com>
2019-02-04 08:47:30 +0300

    few updates

--- js/main.js
+++ js/main.js
@@ -4,7 +4,20 @@ $(document).ready(function(){
   var slider = $('.header-slider');
 
   slider.owlCarousel({
-    items: 1,
-    loop: true
+    items: 1.3,
+    loop: true,
+    dots: true,
+    dostEach: true,
+    dotsContainer: '.count_dots',
+    nav: true,
+    navText: [
+      '<img src="../img/arl.png">',
+      '<img src="../img/arr.png">'
+    ],
+    smartSpeed: 700,
+    margin: 15,
+    onTranslated: function() {
+      $('.count_b').text('0' + (this.items().length));
+    }
   });
 });
